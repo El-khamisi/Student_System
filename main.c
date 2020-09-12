8c2b2d43249527c62053808175a0630355f22853
ahmedtika74 <tekaliker@gmail.com>
2020-09-13 00:32:10 +0200

    add the main page code

--- main.c
+++ main.c
@@ -15,25 +15,31 @@ int main(void)
     while (1)
     {
         // read a number form user to Login to System
-        int choose; 
+        int choose;
+		
+		printf("Welcome ..!\nChoose your account type to login (0 to terminate) :\n1) Student\t2)Admin : ");
         scanf("%d", &choose);
-
 
         switch ( choose )
         {
-        case 1:
-            /* code */
-            break;
+			
+			case 0:
+				exit(0);
+				break;
+				
+			case 1:
+				student();
+				break;
 
-        case 2:
-            /* code */
-            break;
-        
-            
-        default:
-            /*code*/
-            break;
-        }
+			case 2:
+				admin();
+				break;
+			
+				
+			default:
+				printf("Choose a correct number ..! (0 to terminate)\n");
+				break;
+		}
     }
 
     return 0;
